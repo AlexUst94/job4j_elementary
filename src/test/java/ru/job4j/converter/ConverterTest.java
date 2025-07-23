4bd6e3b8b77152d68e26bf3f08dd1d14ea8b48c8
adf88 <adf8840@gmail.com>
2025-07-23 13:42:24 +0300

    исправление

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.converter;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ConverterTest {
@@ -16,5 +17,11 @@ class ConverterTest {
 
     @Test
     void whenConvert200RblThen3dot3333Euro() {
+        float input = 200;
+        float expected = 3.3333f;
+        float output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+
     }
 }
