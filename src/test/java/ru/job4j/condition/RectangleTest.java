8de897d6eb185e7ab3ffe6cc3dff727d7aef75e0
adf88 <adf8840@gmail.com>
2025-09-09 16:10:04 +0300

    исправление тестов

--- src/test/java/ru/job4j/condition/RectangleTest.java
+++ src/test/java/ru/job4j/condition/RectangleTest.java
@@ -1,34 +1,33 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
 
 class RectangleTest {
 
     @Test
-    void whenLength6AndWidth18ThenDiagonal18dot97() {
-        double length = 6;
-        double width = 18;
-        double expected = 18.97;
+    void whenLength3AndWidth4ThenDiagonal5() {
+        double length = 3;
+        double width = 4;
+        double expected = 5.0;
         double output = Rectangle.diagonal(length, width);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenLength22AndWidth40ThenDiagonal45dot65() {
-        double length = 22;
-        double width = 40;
-        double expected = 45.65;
+    void whenLength5AndWidth12ThenDiagonal13() {
+        double length = 5;
+        double width = 12;
+        double expected = 13.0;
         double output = Rectangle.diagonal(length, width);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
-    void whenLength4AndWidth5ThenDiagonal6() {
-        double length = 4;
-        double width = 5;
-        double expected = 6.0;
+    void whenLength7AndWidth24ThenDiagonal25() {
+        double length = 7;
+        double width = 24;
+        double expected = 25.0;
         double output = Rectangle.diagonal(length, width);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -2,26 +2,29 @@ package ru.job4j.converter;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.offset;
 
 class ConverterTest {
 
     @Test
-    void whenConvert140RblThen2Euro() {
-        float input = 140;
-        float expected = 2;
-        float output = Converter.rubleToEuro(input);
-        float value = 0.0001f;
-        assertThat(output).isEqualTo(expected, withPrecision(value));
+    void whenConvert140RblThen1dot5555Euro() {
+        double input = 140;
+        double expected = 1.5555;
+        double output = Converter.rubleToEuro(input);
+        double precision = 0.0001;
+
+
+        assertThat(output).isCloseTo(expected, offset(precision));
     }
 
     @Test
-    void whenConvert200RblThen3dot3333Euro() {
-        float input = 200;
-        float expected = 3.3333f;
-        float output = Converter.rubleToEuro(input);
-        float value = 0.0001f;
-        assertThat(output).isEqualTo(expected, withPrecision(value));
+    void whenConvert200RblThen2dot2222Euro() {
+        double input = 200;
+        double expected = 2.2222;
+        double output = Converter.rubleToEuro(input);
+        double precision = 0.0001;
 
+        assertThat(output).isCloseTo(expected, offset(precision));
     }
 }
