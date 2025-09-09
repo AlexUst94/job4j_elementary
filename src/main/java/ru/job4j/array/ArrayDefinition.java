2dcce8059614ddda0867e5b0ae554bfacb374f79
adf88 <adf8840@gmail.com>
2025-09-09 17:34:14 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
 
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Andrei Hincu";
+        names[2] = "Korobeinikov Stas";
+        names[3] = "Esipov Alexey";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
