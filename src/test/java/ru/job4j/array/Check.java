d319301d056825f571ced3a0842a8675306b3410
adf88 <adf8840@gmail.com>
2025-10-07 13:57:18 +0300

    Массив заполнен true или false [#505232]

---
+++ src/test/java/ru/job4j/array/Check.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        boolean standard = data[0];
+
+        for (int i = 1; i < data.length; i++) {
+
+            if (data[i] != standard) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+
+    }
+}
